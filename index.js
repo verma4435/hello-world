c7be5083b3ee8e106dedf672398245c843fba2e0
verma4435 <47627146+verma4435@users.noreply.github.com>
2026-05-15 18:01:15 +0530

    Update response message in root route

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.status(200).json({ msg: 'Hello world1' }));
+app.get('/', (req, res) => res.status(200).json({ msg: 'Hello world1 - from test' }));
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server is running at ${PORT}`));
