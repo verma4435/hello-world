9febe6e9fc149690abc06736d0ff2d14e3106177
verma4435 <47627146+verma4435@users.noreply.github.com>
2026-05-15 18:22:55 +0530

    greet request

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => res.status(200).json({ msg: 'Hello world1 - from t1' }));
+app.get('/:name', (req, res) => res.status(200).json({msg: `well done ${req.params.name || 'user'}`}))
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server is running at ${PORT}`));
